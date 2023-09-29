c8a1b82fb41568a51441a9e3bc225d452eda4e1d
Minh Nguyen <ngocminhit2000@gmail.com>
2023-09-29 18:03:21 +0700

    Initial Project

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {screens, delivery_time, ratings, tags} from '../../constants';
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
